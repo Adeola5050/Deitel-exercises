c56843d37f66d3ee65b9e7354cb2ea51fd6399d6
Adeola Olarewaju <adeolaesther008@gmail.com>
2021-11-25 09:39:23 +0100

    Chapter two added

---
+++ Chapter two/src/Circle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Circle {
+        public static void main(String[] args){
+            Scanner scanner= new Scanner(System.in);
+
+            System.out.println("ENTER A RADIUS VALUE");
+            int radius= scanner.nextInt();
+
+            double constantPI= Math.PI;
+
+            System.out.printf("Area of the circle is %f%n",constantPI*radius*radius);
+
+            System.out.printf("Circumference of the circle is %f%n",2*constantPI*radius);
+
+            System.out.printf("Diameter of the circle is %d%n",2*radius);
+
+        }
+    }
+
+
